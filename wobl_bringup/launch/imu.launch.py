be278588e98410e266b1cff4af3d63c7c45e6986
Nikolai Nojgaard <nnoej10@gmail.com>
2025-07-30 19:24:03 +0200

    Fixup imu.launch.py

--- wobl_bringup/launch/imu.launch.py
+++ wobl_bringup/launch/imu.launch.py
@@ -5,16 +5,20 @@ def generate_launch_description():
     return LaunchDescription([
         # IMU Publisher Node (replace with your actual IMU driver)
         Node(
-            package='real',
+            package='wobl_hardware',
             executable='imu_node',
             name='imu_publisher',
         ),
-
-        # Throttle Node from topic_tools
         Node(
             package='topic_tools',
             executable='throttle',
             name='imu_network',
             arguments=['messages', 'imu/data', '20.0', 'imu/data_network']
+        ),
+        Node(
+            package="foxglove_bridge",
+            executable="foxglove_bridge",
+            name="foxglove_bridge",
+            output="screen",
         )
     ])
