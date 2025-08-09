f4bb732d08e1f389ae6cffe00dc974ca20d23eca
Nikolai Nojgaard <nnoej10@gmail.com>
2025-08-09 10:49:12 +0200

    Add real debug launch profile

---
+++ wobl_bringup/launch/real_debug.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="wobl_hardware",
+                executable="imu_node",
+                name="imu_node",
+                output="screen",
+            ),
+            Node(
+                package="wobl_hardware",
+                executable="servo_node",
+                name="servo_node",
+                output="screen",
+            ),
+            Node(
+                package="wobl_controllers",
+                executable="pid_balance_controller",
+                name="pid_balance_controller",
+                output="screen",
+            ),
+            Node(
+                package="topic_tools",
+                executable="throttle",
+                name="imu_remote_publisher",
+                arguments=["messages", "imu/data", "20.0", "remote/imu/data"],
+            ),
+            Node(
+                package="topic_tools",
+                executable="throttle",
+                name="joint_state_remote_publisher",
+                arguments=["messages", "joint_states", "20.0", "remote/joint_states"],
+            ),
+            Node(
+                package="topic_tools",
+                executable="throttle",
+                name="joint_command_remote_publisher",
+                arguments=["messages", "joint_commands", "20.0", "remote/joint_commands"],
+            ),
+            Node(
+                package="foxglove_bridge",
+                executable="foxglove_bridge",
+                name="foxglove_bridge",
+                output="screen",
+            )
+        ]
+    )
