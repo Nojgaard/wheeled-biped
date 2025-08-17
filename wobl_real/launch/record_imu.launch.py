256202f95f7b25cc0718ecc23bf97dda28a0495a
Nikolai Nojgaard <nnoej10@gmail.com>
2025-08-17 21:55:33 +0200

    update launch

--- wobl_real/launch/record_imu.launch.py
+++ wobl_real/launch/record_imu.launch.py
@@ -4,28 +4,21 @@ from launch.actions import TimerAction, ExecuteProcess
 from wobl_msgs.msg import Topics
 
 def generate_launch_description():
-    # Node to enable servos
-    enable_servos_node = Node(
-        package='rclpy',
-        executable='publisher',
-        name='enable_servos_pub',
-        arguments=[
-            '--topic', 'servo/enabled',
-            '--msg-type', 'std_msgs/msg/Bool',
-            '--publish', 'data:=true'
+    # Publish to enable servos using ros2 topic pub
+    enable_servos_pub = ExecuteProcess(
+        cmd=[
+            'ros2', 'topic', 'pub', '--once',
+            'servo/enabled', 'std_msgs/msg/Bool', '{data: true}'
         ],
         output='screen'
     )
 
-    # Node to send joint command (all zeros)
-    joint_command_node = Node(
-        package='rclpy',
-        executable='publisher',
-        name='joint_command_pub',
-        arguments=[
-            '--topic', Topics.JOINT_COMMAND,
-            '--msg-type', 'wobl_msgs/msg/JointCommand',
-            '--publish', 'position:=[0.0,0.0,0.0,0.0]', 'velocity:=[1.0,1.0,0.0,0.0]'
+    # Publish joint command (all zeros) using ros2 topic pub
+    joint_command_pub = ExecuteProcess(
+        cmd=[
+            'ros2', 'topic', 'pub', '--once',
+            '/joint_command', 'wobl_msgs/msg/JointCommand',
+            '{position: [0.0, 0.0, 0.0, 0.0], velocity: [0.0, 0.0, 0.0, 0.0]}'
         ],
         output='screen'
     )
@@ -47,7 +40,7 @@ def generate_launch_description():
             executable='servo_node',
             name='servo_node',
         ),
-        TimerAction(period=2.0, actions=[enable_servos_node]),
-        TimerAction(period=1, actions=[joint_command_node]),
+        TimerAction(period=2.0, actions=[enable_servos_pub]),
+        TimerAction(period=1.0, actions=[joint_command_pub]),
         TimerAction(period=1.0, actions=[rosbag_record])
     ])
